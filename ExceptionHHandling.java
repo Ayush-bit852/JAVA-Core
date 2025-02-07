879df5bdad3b477da8cb673ede2ce33cfdcf5f9d
Ayush Sharma <77086486+Ayush-bit852@users.noreply.github.com>
2025-02-07 11:13:23 +0530

    Exception Handlingg

--- ExceptionHHandling.java
+++ ExceptionHHandling.java
@@ -83,3 +83,68 @@ public class ExceptionHHandling {
 //         }
 //     }
 // }
+
+
+
+////////------------///////////------------///////////////-----------////////-------///--------/-
+                    //Threads//
+//class MyThread extends Thread
+//{
+//    // initiated run method for Thread
+//    public void run()
+//    {
+//        String str = "Thread Started Running...";
+//        System.out.println(str);
+//    }
+//}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Running the Threads in Java
+//
+//import java.io.*;
+//        import java.util.*;
+//
+//// Method 1 - Thread Class
+//class ThreadImpl extends Thread
+//{
+//    // Method to start Thread
+//    @Override
+//    public void run()
+//    {
+//        String str = "Thread Class Implementation Thread"
+//                + " is Running Successfully";
+//        System.out.println(str);
+//    }
+//}
+//
+//// Method 2 - Runnable Interface
+//class RunnableThread implements Runnable
+//{
+//    // Method to start Thread
+//    @Override
+//    public void run()
+//    {
+//        String str = "Runnable Interface Implementation Thread"
+//                + " is Running Successfully";
+//        System.out.println(str);
+//    }
+//
+//}
+//
+//public class Geeks
+//{
+//    public static void main(String[] args)
+//    {
+//
+//        // Method 1 - Thread Class
+//        ThreadImpl t1 = new ThreadImpl();
+//        t1.start();
+//
+//        // Method 2 - Runnable Interface
+//        RunnableThread g2 = new RunnableThread();
+//        Thread t2 = new Thread(g2);
+//        t2.start();
+//
+//    }
+//}
