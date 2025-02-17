2b073d46a2bfd12551980b83ca88f81c8b21dfab
Ayush Sharma <77086486+Ayush-bit852@users.noreply.github.com>
2025-02-17 13:31:59 +0530

    Stream Api Examples

--- StreamApiExample1.java
+++ StreamApiExample1.java
@@ -6,15 +6,12 @@ public class StreamApiExample1 {
     public static void main(String [] args){
 
         List<Integer>nums = Arrays.asList(2,3,4,5);
-//        nums.stream()
-//                .filter(num -> num%2==0)  // Filters even NUmbers
-//                .map(num -> num*num)
-//                .forEach(System.out::println);
+        nums.stream()
+                .filter(num -> num % 2 == 0)  // Filters even NUmbers
+                .map(num -> num * num)
+                .forEach(System.out::println);
 
-        Stream<Integer> S1 = nums.stream();
-        S1.filter(S2 -> S2%2==0);
-        S1.map(S2 -> S2*S2);
-        S1.forEach(System.out::println);
+
 
 
 
--- StreamApiExample2.java
+++ StreamApiExample2.java
@@ -7,10 +7,10 @@ public class StreamApiExample2 {
     public static void main (String [] args){
 
         List<Integer>num = Arrays.asList(2,4,6);
-        Stream<Integer> S2 = num.stream();
-        S2.filter(S3 -> S3%2==0);
-        S2.map(S3-> S3*S3);
-        S2.forEach(System.out::println);
+        Stream<Integer> S1 = num.stream();
+        Stream<Integer> S2 = S1.filter(n -> n%2==0);
+        Stream<Integer> S3 = S2.map(n-> n*n);
+        S3.forEach(System.out::println);
 
     }
 }
